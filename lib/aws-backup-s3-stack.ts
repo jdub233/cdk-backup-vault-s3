4ea99a928d27d447685956d773d0062b8b994adb
Jonathan Williams <tamlin@mac.com>
2024-05-02 11:05:04 -0400

    add an option for an existing bucket arn
    that comes from the .env

--- lib/aws-backup-s3-stack.ts
+++ lib/aws-backup-s3-stack.ts
@@ -5,25 +5,40 @@ import {
   StackProps,
 } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { config } from 'dotenv';
 
 import { createBackupRole } from "./backup-role";
+
+config();
+
+function createBucket(scope: Construct, id: string): aws_s3.Bucket {
+  return new aws_s3.Bucket(scope, id, {
+    blockPublicAccess: {
+      blockPublicAcls: true,
+      blockPublicPolicy: true,
+      ignorePublicAcls: true,
+      restrictPublicBuckets: true,
+    },
+    enforceSSL: true,
+    publicReadAccess: false,
+    encryption: aws_s3.BucketEncryption.S3_MANAGED,
+    versioned: true,
+  });
+}
+
 
 export class AwsBackupS3Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const bucket = new aws_s3.Bucket(this, "testBucket", {
-      blockPublicAccess: {
-        blockPublicAcls: true,
-        blockPublicPolicy: true,
-        ignorePublicAcls: true,
-        restrictPublicBuckets: true,
-      },
-      enforceSSL: true,
-      publicReadAccess: false,
-      encryption: aws_s3.BucketEncryption.S3_MANAGED,
-      versioned: true,
-    });
+    // Get the bucket ARN from the environment variables, or use a default null value.
+    const bucketArn = process.env.BUCKET_ARN || null;
+
+
+    // If there is a valid bucket arn, use that bucket as resource to be backed up.
+    const bucket: aws_s3.IBucket = bucketArn
+      ? aws_s3.Bucket.fromBucketArn(this, "testBucket", bucketArn)
+      : createBucket(this, "testBucket");     // Otherwise, create a new 'test' bucket to use with the backup vault.
 
     // Create the backup role.
     const backupRole = createBackupRole(this);
